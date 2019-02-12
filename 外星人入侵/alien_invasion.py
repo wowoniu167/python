d6b48565b8f404dc3fb2ab02224bc6c9215da302
叶立鹏 <yelipeng167@outlook.com>
2019-02-12 18:16:06 +0800

    完成飞船移动

--- 外星人入侵/alien_invasion.py
+++ 外星人入侵/alien_invasion.py
@@ -1,16 +1,19 @@
-import sys
 import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
 def run_game():
     """初始化游戏并创建一个屏幕对象"""
-pygame.init()
-screen = pygame.display.set_mode((1200, 800))
-pygame.display.set_caption("Alien Invasion")
-# 开始游戏的主循环
-while True:
-    # 监视键盘和鼠标事件
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-    # 让最近绘制的屏幕可见
-    pygame.display.flip()
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    # 创建一艘飞船
+    ship = Ship(ai_settings,screen)
+    # 开始游戏的主循环
+    while True:
+        # 监视键盘和鼠标事件
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 run_game()

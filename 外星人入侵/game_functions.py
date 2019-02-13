51c52f4fd9e8a42b76a90155036fca0837ecad35
叶立鹏 <yelipeng167@outlook.com>
2019-02-13 12:59:30 +0800

    调试子弹

--- 外星人入侵/game_functions.py
+++ 外星人入侵/game_functions.py
@@ -7,7 +7,7 @@ def check_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
            sys.exit()
-def update_screen(ai_settings, screen, ship,bullets):
+def update_screen(ai_settings, screen, ship, bullets):
     """更新屏幕上的图像，并切换到新屏幕"""
     # 每次循环时都重绘屏幕
     screen.fill(ai_settings.bg_color)
@@ -17,23 +17,21 @@ def update_screen(ai_settings, screen, ship,bullets):
     # 让最近绘制的屏幕可见
     pygame.display.flip()
 
-def check_keydown_events(event, ai_settins,screen,ship, bullets):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """响应按键"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        new_bullet = Bullet(ai_settins, screen, ship)
-        bullets.add(new_bullet)
-
+        fire_bullet(ai_settings, screen, ship, bullets)
 def check_keyup_events(event, ship):
     """响应按键"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
-def check_events(ai_settins,screen,ship,bullets):
+def check_events(ai_settins, screen, ship, bullets):
     """响应按键和鼠标事件"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -42,3 +40,17 @@ def check_events(ai_settins,screen,ship,bullets):
             check_keydown_events(event, ai_settins, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
+def update_bullets(bullets):
+    """更新子弹的位置，并删除已消失的子弹"""
+    # 每次循环时都重绘屏幕
+    bullets.update()
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+    #            print(len(bullets))
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """如果还没有到达限制，就发射一颗子弹"""
+    # 创建新子弹，并将其加入到编组bullets中
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)

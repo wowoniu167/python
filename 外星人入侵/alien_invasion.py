a0444fa54ee674fe04faa7e14b9023591665963a
叶立鹏 <yelipeng167@outlook.com>
2019-02-14 18:11:36 +0800

    alien

--- 外星人入侵/alien_invasion.py
+++ 外星人入侵/alien_invasion.py
@@ -1,6 +1,7 @@
 import pygame
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 from bullet import Bullet
 from pygame.sprite import  Group
@@ -12,6 +13,8 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     # 创建一艘飞船
     ship = Ship(ai_settings,screen)
+    # 创建一个外星人
+    alien = Alien(ai_settings, screen)
     #创建一个子弹组
     bullets = Group()
     # 开始游戏的主循环
@@ -20,5 +23,5 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
 run_game()
--- 外星人入侵/game_functions.py
+++ 外星人入侵/game_functions.py
@@ -2,18 +2,14 @@ import sys
 import pygame
 from bullet import Bullet
 
-def check_events():
-    """响应按键和鼠标事件"""
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-           sys.exit()
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, alien, bullets):
     """更新屏幕上的图像，并切换到新屏幕"""
     # 每次循环时都重绘屏幕
     screen.fill(ai_settings.bg_color)
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
+    alien.blitme()
     # 让最近绘制的屏幕可见
     pygame.display.flip()
 
@@ -25,6 +21,8 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         fire_bullet(ai_settings, screen, ship, bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
 def check_keyup_events(event, ship):
     """响应按键"""
     if event.key == pygame.K_RIGHT:
